3d305c29896e1f0a612e78dd81b66e23af8b9da6
shishirRsiam <shishir.siam01@gmail.com>
2025-06-10 17:35:22 +0600

    update Final Exam file

--- 1. Introduction to C Programming/Course Exam/Final Exam/Tiger_Pathan.c
+++ 1. Introduction to C Programming/Course Exam/Final Exam/Tiger_Pathan.c
@@ -1,38 +1,38 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int n;
-    scanf("%d",&n);
-    for (int s=0;s<n;s++)
+    scanf("%d", &n);
+    for (int s = 0; s < n; s++)
     {
         int t;
-        scanf("%d",&t);
+        scanf("%d", &t);
         char a[100001];
-        scanf("%s",a);
-        int ln=strlen(a);
-        int Tiger=0,Pathan=0;
-        for (int i=0;i<ln;i++)
+        scanf("%s", a);
+        int ln = strlen(a);
+        int Tiger = 0, Pathan = 0;
+        for (int i = 0; i < ln; i++)
         {
-            if(a[i]=='T')
+            if (a[i] == 'T')
             {
                 Tiger++;
             }
-            if (a[i]=='P')
+            if (a[i] == 'P')
             {
                 Pathan++;
             }
         }
-        if(Tiger==Pathan)
+        if (Tiger == Pathan)
         {
             printf("Draw\n");
         }
-        if (Pathan>Tiger)
+        if (Pathan > Tiger)
         {
             printf("Pathaan\n");
         }
-        if (Tiger>Pathan)
+        if (Tiger > Pathan)
         {
             printf("Tiger\n");
         }
